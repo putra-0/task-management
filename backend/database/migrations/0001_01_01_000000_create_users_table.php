3690f1e92b5df09242633037b01ceb479a826338
Putra-0 <putrasa1219@gmail.com>
2025-12-17 20:47:27 +0700

    Add email_verified_at column and update User model casts

--- backend/database/migrations/0001_01_01_000000_create_users_table.php
+++ backend/database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,9 +13,11 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->uuid('uuid')->unique();
             $table->string('name', 100);
             $table->string('username', 100)->unique();
             $table->string('email', 100)->unique();
+            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->timestamps();
         });
