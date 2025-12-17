2dbf26a450f34ce21d530cbc7b5e36225704dc5d
Putra-0 <putrasa1219@gmail.com>
2025-12-17 21:36:06 +0700

    Remove username field from users table migration

--- backend/database/migrations/0001_01_01_000000_create_users_table.php
+++ backend/database/migrations/0001_01_01_000000_create_users_table.php
@@ -15,7 +15,6 @@ return new class extends Migration
             $table->id();
             $table->uuid('uuid')->unique();
             $table->string('name', 100);
-            $table->string('username', 100)->unique();
             $table->string('email', 100)->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
