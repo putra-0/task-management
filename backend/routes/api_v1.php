0ee2cde4786338266583dfc6058d26b5d2c8bd51
Putra-0 <putrasa1219@gmail.com>
2025-12-18 09:38:38 +0700

    Implement authentication features with registration, login, OTP verification, and user profile retrieval

--- backend/routes/api_v1.php
+++ backend/routes/api_v1.php
@@ -1,1 +1,17 @@
 <?php
+
+use App\Http\Controllers\Api\V1\AuthController;
+use Illuminate\Support\Facades\Route;
+
+Route::controller(AuthController::class)
+    ->prefix('auth')
+    ->group(function () {
+        Route::withoutMiddleware('verify.user')->group(function () {
+            Route::post('register', 'register');
+            Route::post('login', 'login');
+            Route::post('verify-otp', 'verifyOtp');
+        });
+
+        Route::post('logout', 'logout')->name('logout');
+        Route::get('profile', 'getProfile');
+    });
